72c1209b34d600d4b1fb81a3b7a8c585751a43f6
sdw283074970 <downwes@gmail.com>
2018-02-21 01:04:12 -0500

    Update and rename Introduction/3.设置开发环境SettingUpTheDevelopmentEnvironment.cs to 1.Introduction/3.设置开发环境SettingUpTheDevelopmentEnvironment.cs

---
+++ 1.Introduction/3.设置开发环境SettingUpTheDevelopmentEnvironment.cs
@@ -0,0 +1,8 @@
+//Q: 要在哪经行ASP.NET开发？
+//A: 当然要在微软自家的Visual Studio中开发。
+
+//Q: 还需要装其他东西吗？
+//A: 装一些有用的插件能明显提升变成效率。点击TOOLS->Extention and Updates->Online->搜索Visual Studio Productivity Power Tools。
+  //再搜索Web Essentials。最后一个三方插件是ReSharper，谁用谁知道，我是看别人用，自己没用过，看起来不错，需要花钱买。
+
+//暂时想到这么多，最后更新2018/02/21

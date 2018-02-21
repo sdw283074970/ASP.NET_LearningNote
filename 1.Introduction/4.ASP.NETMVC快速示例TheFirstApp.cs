1827b2d2a5df92ab7084f6f53abb3c765261c7f4
sdw283074970 <downwes@gmail.com>
2018-02-21 01:15:55 -0500

    Update and rename Introduction/4.ASP.NETMVC快速示例TheFirstApp.cs to 1.Introduction/4.ASP.NETMVC快速示例TheFirstApp.cs

---
+++ 1.Introduction/4.ASP.NETMVC快速示例TheFirstApp.cs
@@ -0,0 +1,20 @@
+//Q: 如何建立一个ASP.NET项目？
+//A: 选择新建项目->Template->Web->ASP.NET Web App，并命名Vidly，然后取消Source Control勾选。再出现的对话框中选择MVC模板，取消勾选HostInTheCloud
+  //选项。至此新项目创建完成。
+
+//Q: 用MVC模板创建的解决方案中有很多文件夹和杂七杂八的文件，这些是干什么用的？
+//A: 以下为各个文件夹/文件的介绍：
+  //1.App_Data文件夹
+  //2.App_Start文件夹
+  //3.Content文件夹
+  //4.Controllers文件夹
+  //5.fonts文件夹
+  //6.Models文件夹
+  //7.Scripts文件夹
+  //8.Views文件夹
+  //9.favicon.ico
+  //10.Global.asax
+  //11.packages.configs
+  //12.Project_Readme.html
+  //13.Startup.cs
+  //14.Web.config

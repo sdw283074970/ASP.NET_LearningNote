15f1d322388f616d90500b7095b90c7e9cce23fe
sdw283074970 <downwes@gmail.com>
2018-03-01 13:25:57 -0500

    Update 3.代码优先的迁移CodeFirstMigrations.cs

--- 3.WorkingWithData/3.代码优先的迁移CodeFirstMigrations.cs
+++ 3.WorkingWithData/3.代码优先的迁移CodeFirstMigrations.cs
@@ -1,34 +1,88 @@
-//本节内容与MicrosoftSQLServer_EntityFramework/3.CodeFirstWorkFlow/3.迁移:启用迁移EnablingMigrations.cs完全一样
+//本节内容为基于MicrosoftSQLServer_EntityFramework/3.CodeFirstWorkFlow/3.迁移:启用迁移EnablingMigrations.cs的衍生
 
-//Q: 到底什么是迁移？
-//A: 在我们使用CFW时，我们的Model不可能一直不变。当我们的Model改变时，需要通知数据库做相应的改变，即同步。其原理为EF能感知Model的变化(主要是与之前所
-  //有的迁移文件经行对比，如果之前没有文件记录，则认为这是新数据库，会将整个Model视为改变)，会先自动生成一个C#语言的变更操作记录，EF再将这一系列操作记录
-  //转换为Sql语言通知数据库执行，然后数据库就与我们的Model同步了。这个过程就叫做迁移(Migration)。
+//Q: 如何在ASP.NET MVC5项目中启用迁移？
+//A: 在PM中输入enable-migrations即可，首次启用后便可不用再启用。启用成功后继续在PM中输入add-migration InitialModel建立初始迁移文件。
 
-//Q: 如何启用迁移？
-//A: 在PM中输入enable-migrations即可启用迁移。启用后，解决方案中会生成一个Migrations的文件夹，里面将储存每一次EF生成的C#语言Model改变记录。
+//Q: 初始迁移文件中建立的"dbo.AspNetRoles""dbo.AspNetUserRoles""dbo.AspNetUsers"这些是哪来的?
+//A: 这些是ASP.NET自动创建的验证表格，用于验证用户的控制权限。我们可以在Models文件夹中找到IdentityModels.cs文件，此文件中有两个类，代码如下：
 
-//Q: 如何初始化迁移？
-//A: 在启用迁移后，我们就要做第一次迁移，即初始化。在已存在的数据库项目基础上的迁移由于没有先前迁移文件记录，EF会将整个Model视为改变，这将产生一个重复
-  //创建数据的问题。为了解决这个问题，只用在第一次建立迁移文件时在PM中输入-IgnoreChanges，即在PM输入add-migration InitialModel -IgnoreCHanges，
-  //则会忽略所有改变，生成一个空的迁移文件。作为结论，在所有基于已存在数据库项目上建立的CodeFirstModel在初始化迁移时必须加上-IgnoreChanges参数。
-  //空的迁移文件代码即迁移类方法含义如下：
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
-    public partial class InitialModel : DbMigration
+namespace Vidly.Models
+{
+    public class ApplicationUser : IdentityUser   //ApplicationUser类，即用户类
     {
-        public override void Up()
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            //Up方法，即更新方法。EF会比较现有Model与之前的迁移文件，然后基于它们的差异生成一个Up方法，执行后EF会将Up方法翻译成Sql文件并让数据库执行。
-            //我们也可以在这里植入一些Sql查询语句来对数据库进行操作。
-        }
-
-        public override void Down()
-        {
-            //Down方法，即还原/降级方法。此方法与Up方法无脑相反，即通过调用这个方法来将数据库还原到调用Up方法之前的状态，EF也会自动生成此方法代码。
-            //但是请切记，任何在Up方法做过变动后都要检查Down方法，千万要保持Down方法与所有Up方法无脑相反，否则数据库GG。
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
         }
     }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>    //DbContext域类，即通往数据库的类，属于ASP.NET EntityFramework
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
+
+//Q: 为什么没有Movie和Customer列表？
+//A: 首先这两个列表还没有被建立，即使存在，DbContext类中没有引用这两个表，所以EntityFramework不知道他们的存在。
+
+//Q: 如何建立表？
+//A: 在DbContext类，在这里即ApplicationDbContext建立表即可，可以将ApplicationDbContext视为PlutoDbContext(在另一个Repository中的例子)。
+  //建立Movies和Customers表代码如下：
   
-  //此外附上做迁移的黄金准则：每当做一次做小规模改变后就要做一次小规模迁移，最好一个改变一次迁移。
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Movie> Movies { get; set; }    //建立电影列表
+        public DbSet<Customer> Customers { get; set; }    //建立顾客列表
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+    
+  //最后可以使用add-migration InitialModel -force强制重新建立初始化迁移。我们可以看到Up()方法中多了新建两个表的方法：
+
+            CreateTable(
+                "dbo.Customers",    //建立Customers表
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Movies",   //建立Movies表
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
+//Q: 现在并没有设置数据库connectionString，在哪里生成数据库文件？迁移同步会通向哪里？
+//A: 可以直接通过update-database生成本地数据库文件而不用建立Microsoft Sever。生成成功后，可在App_Data文件夹中找到扩展名为.mdf的数据库文件，
+  //前提条件是要显示所有文件，该数据库文件默认为隐藏。双击就可以打开数据库浏览器。
 
 //最后更新2018/03/01

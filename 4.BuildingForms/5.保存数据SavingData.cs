0c06944d94a8bd8341b08bf1e7db78c3a50528e8
sdw283074970 <downwes@gmail.com>
2018-03-12 20:39:54 -0700

    Update and rename 5.SavingData.cs to 5.保存数据SavingData.cs

---
+++ 4.BuildingForms/5.保存数据SavingData.cs
@@ -0,0 +1,15 @@
+//Q: 如何将数据保存至数据库中？
+//A: 请参照另一个Repository：MicrosoftSQLServer_EntityFramework/7.UpdatingData 中的所有章节。
+  //如，项目需求为将客户填写的表单数据保存至数据库中，Creat()方法(Action)的代码如下：
+
+        [HttpPost]
+        public ActionResult Creat(NewCustomerViewModel viewModel)
+        {
+            _context.Customers.Add(viewModel.Customer);   //试用Add()方法将ModelBinding保存的递数据添加到数据库中
+            _context.SaveChanges();   //保存
+            return RedirectToAction("Index", "Customers");    //重导向
+        }
+
+  //就是这么简单。
+
+//暂时想到这么多，最后更新2018/03/12

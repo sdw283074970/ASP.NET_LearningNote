e7c65924c8fa766a0393d4c1e18a0d699a1ea607
sdw283074970 <downwes@gmail.com>
2018-03-15 13:15:21 -0700

    Update 1.添加验证AddingValidation.cs

--- 5.ImplementingValidation/1.添加验证AddingValidation.cs
+++ 5.ImplementingValidation/1.添加验证AddingValidation.cs
@@ -1,5 +1,5 @@
 //Q: 什么是Validation？
-//A: Validation即验证，验证的根本目的在于确保整个程序按照逻辑运行，不符合逻辑的执行命令(即没有被验证的执行命令)将会中断程序，抛出Invalidation的异常。
+//A: Validation即验证，验证的根本目的在于确保整个程序按照逻辑运行。在没有验证的情况下，如果执行命令不符合逻辑，程序将会中止并抛出Invalidation的异常。
   //如，某一个字段要求不能为空，如果程序为一个空的输入放行，则会产生一系列连锁效应，让整个程序无法正常执行。所以我们需要一道“安检”过程，确保程序的执行
   //符合逻辑。
   
@@ -35,11 +35,11 @@ namespace Vidly.Models
 
   //2.添加验证逻辑。在ASP.NET中，有一个静态类布尔属性ModelState.IsValid来储存模型的验证状态，通过这个属性可以判断整个程序的运行是否符合逻辑。如果
     //在新建或者编辑Customer的页面中，Name字段不符合逻辑要求(如输入为空)，说明程序存在不符合逻辑的情况，ModelState.IsValid的值为False。这样，
-    //我们可以通过ModelState.IsValid的值判断程序是否有问题。如果有问题，就需要返回带错误信息的视图给用户。
+    //我们可以通过ModelState.IsValid的值判断程序是否有问题。如果有问题，就需要返回带错误信息的视图给用户，从而阻止程序中止。
     
     //如项目需求为在添加顾客页面和编辑页面中，即使用户输入不符合逻辑的数据，程序也不能崩溃，还要能告诉用户出错的地方。
     
-    //据分析，相关的视图为“CustomerForm”，相关的Controllor和Action为"Customer/Save"。我们用修改Save()方法即可判断程序的验证性。代码如下：
+    //据分析，相关的视图为“CustomerForm”，相关的Controllor和Action为"Customer/Save"。我们只用修改Save()方法即可判断程序的验证性。代码如下：
     
         [HttpPost]
         public ActionResult Save(CustomerFormViewModel viewModel)

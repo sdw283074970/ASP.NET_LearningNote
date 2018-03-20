e14bb8ddd604829f2470cfb0f3ede8b096c95321
sdw283074970 <downwes@gmail.com>
2018-03-20 10:36:46 -0700

    Update 2.RESTfulConvention.cs

--- 6.BuildingRESTfulServicesWithASP.NETWebAPI/2.RESTfulConvention.cs
+++ 6.BuildingRESTfulServicesWithASP.NETWebAPI/2.RESTfulConvention.cs
@@ -1,1 +1,6 @@
-//
+//Q: 什么是RESTfulConvention？
+//A: REST指Representational State Tranfer，这是一种设计约定，遵守这种设计约定设计的API我们称之为RESTful API。
+
+//Q: REST约定的具体内容是什么？
+//A: 具体内容为将访问资源的动作(Action)省略，具体执行动作交由HTTP请求来划定。如之前的例子，在不使用RESTful风格API的情况下，访问Customer资源的默认
+  //页面是通过路由/Customers/Index或直接/Customers

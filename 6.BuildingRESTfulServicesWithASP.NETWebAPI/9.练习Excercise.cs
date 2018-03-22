1bb2d66415891cb89611bfc1394500cb5f09dc5a
sdw283074970 <downwes@gmail.com>
2018-03-22 12:31:30 -0700

    Update and rename 9.联系Excercise.cs to 9.练习Excercise.cs

--- 6.BuildingRESTfulServicesWithASP.NETWebAPI/9.练习Excercise.cs
+++ 6.BuildingRESTfulServicesWithASP.NETWebAPI/9.练习Excercise.cs
@@ -74,7 +74,7 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        // GET /api/movies
+        // 需求1：GET /api/movies
         public IHttpActionResult GetMovies()
         {
             if (!ModelState.IsValid)
@@ -85,7 +85,7 @@ namespace Vidly.Controllers.Api
             return Ok(movies);
         }
 
-        // GET /api/movies/{id}
+        // 需求2：GET /api/movies/{id}
         public IHttpActionResult GetMovie(int id)
         {
             if (!ModelState.IsValid)
@@ -101,9 +101,10 @@ namespace Vidly.Controllers.Api
             return Ok(movieDto);
         }
         
-        // POST /api/movies
+        // 需求3：POST /api/movies
         [HttpPost]
-        public  IHttpActionResult CreatMovies(IEnumerable<MovieDto> moviesDto)
+        //为满足需求3，用户端发送的请求包中的数据为一列表，这个Action需要将整个列表一次性都添加进数据库中
+        public  IHttpActionResult CreatMovies(IEnumerable<MovieDto> moviesDto)    //用户端传入IEnumerable<T>类型的对象
         {
             var movies = new List<Movie>();
             int i = 0;
@@ -111,25 +112,27 @@ namespace Vidly.Controllers.Api
             if (!ModelState.IsValid)
                 return BadRequest();
 
-            foreach(var movieDto in moviesDto)
+            foreach(var movieDto in moviesDto)    //迭代用户传入的IEnumerable<T>中的所有对象
             {
-                var movie = Mapper.Map<MovieDto, Movie>(movieDto);
-                movies.Add(movie);
+                var movie = Mapper.Map<MovieDto, Movie>(movieDto);    //将每个movieDto对象连接转换成movie
+                movies.Add(movie);    //将movie对象添加到movies列表中
             }
 
-            _context.Movies.AddRange(movies);
-            _context.SaveChanges();
+            _context.Movies.AddRange(movies);   //将movies列表添加进数据库中
+            _context.SaveChanges();   //保存
 
+            //按照默认约定，需要向用户端返回新建的对象，包括数据库为其每个对象分配的id
             foreach(var movieDto in moviesDto)
             {
-                movieDto.Id = movies[i].Id;
+                movieDto.Id = movies[i].Id;   //为每一个movieDto对象更新从数据看返回的id信息
                 i = i + 1;
             }
 
+            //以列表的形式返回所建立对象，其Uri为/api/movies/{起始新对象id}:{末尾新对象id}
             return Created(new Uri(Request.RequestUri + "/" + movies[0].Id + ":" + movies[movies.Count - 1].Id), moviesDto);
         }
 
-        // PUT /api/Movies/{id}
+        // 需求4：PUT /api/Movies/{id}
         [HttpPut]
         public void UpdateMovie (int id, MovieDto movieDto)
         {
@@ -141,12 +144,12 @@ namespace Vidly.Controllers.Api
             if (movieInDb == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            Mapper.Map(movieDto, movieInDb);
+            Mapper.Map(movieDto, movieInDb);    //可省略Mapper.Map<MovieDto, Movie>(movieDto, movieInDb)中的<MovieDto, Movie>
 
             _context.SaveChanges();
         }
 
-        // DELETE /api/Movies/{id}
+        // 需求5：DELETE /api/Movies/{id}
         [HttpDelete]
         public void DeleteMovie(int id)
         {
@@ -163,3 +166,34 @@ namespace Vidly.Controllers.Api
         }
     }
 }
+
+  //据说，在处理PUT请求的时候会报错，发送错误的地方在：
+  
+            Mapper.Map(movieDto, movieInDb);
+
+  //据分析，报错原因为这个方法企图将movieDto.Id赋予到moviesInDb.Id中，由于moviesInDb.Id已经存在于数据库了，作为自动生成的主键，Id属性不允许更改。
+    //之前Created请求能为Id赋值是因为这是新建过程，数据库将无视带值的Id，而会重新为其分配一个Id。
+
+  //解决更新数据库报错的方案为，在MappingProfile中设置忽略Id的更新，其代码如下：
+
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(m => m.Id, opt => opt.Ignore());    //忽略Id的更新
+            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());    //忽略Id的更新
+            Mapper.CreateMap<MovieDto, Movie>();
+        }
+    }
+}
+
+  //但是目前即使不说明忽略更新，AutoMapper也会自动在PUT请求中忽略Id的更新，也就是说，目前版本似乎不针对这个问题做处理也不会得到报错。
+
+//暂时想到这么多，最后更新2018/03/22
